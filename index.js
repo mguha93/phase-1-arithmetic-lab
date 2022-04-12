040306cb5d70fe7efbb781ad08a26f13dace3a93
Moushumi Guha <moushumiguha@yahoo.com>
2022-04-12 00:14:43 -0400

    commit tree ae7ccf21b06b80aa1936325e3d4c3e521cff7f5a

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+    //return num1 * num2;
+    // expected output: 62
 
-// Write your code here
+
+const random = Math.floor(Math.random()*10);
+
+const num3 = 16
+const num4 = 12
+const mod = num3 % num4
+    // expected output: 4
+ 
+const max = Math.max(10,20);
